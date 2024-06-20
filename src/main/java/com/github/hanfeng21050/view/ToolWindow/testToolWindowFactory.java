e127d661254d68a70cea44aa39260ab29e81479f
hanfeng32305 <hanfeng32305@hundsun.com>
2024-06-21 01:08:52 +0800

    2020版本更新

--- src/main/java/com/github/hanfeng21050/view/ToolWindow/testToolWindowFactory.java
+++ src/main/java/com/github/hanfeng21050/view/ToolWindow/testToolWindowFactory.java
@@ -3,14 +3,10 @@ package com.github.hanfeng21050.view.ToolWindow;
 import com.github.hanfeng21050.utils.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowAnchor;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import javax.swing.*;
 
 public class testToolWindowFactory implements ToolWindowFactory {
     private static final String DISPLAY_NAME = "";
@@ -22,39 +18,26 @@ public class testToolWindowFactory implements ToolWindowFactory {
 
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-
+        // 在这里初始化工具窗口内容
+        init(project, toolWindow);
     }
 
-    @Override
-    public void init(@NotNull ToolWindow toolWindow) {
-        ToolWindowFactory.super.init(toolWindow);
-        Project project = toolWindow.getProject();
+    public void init(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         ViewBars viewPanel = new ViewBars(project, toolWindow);
         // 获取内容工厂的实例
-        ContentFactory contentFactory = ContentFactory.getInstance();
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
 
-        //创建一个Content，也就是toolwindow里面的一个tab页
+        // 创建一个Content，也就是toolwindow里面的一个tab页
         Content content = contentFactory.createContent(viewPanel, "执行日志", false);
-        //将Content加入到toolwindow中
+        // 将Content加入到toolwindow中
         toolWindow.getContentManager().addContent(content);
 
         // 设置Logger的实例和工具窗口
         Logger.setViewBarsInstance(viewPanel, toolWindow);
-
     }
 
     @Override
     public boolean shouldBeAvailable(@NotNull Project project) {
         return ToolWindowFactory.super.shouldBeAvailable(project);
-    }
-
-    @Override
-    public @Nullable ToolWindowAnchor getAnchor() {
-        return ToolWindowFactory.super.getAnchor();
-    }
-
-    @Override
-    public @Nullable Icon getIcon() {
-        return ToolWindowFactory.super.getIcon();
     }
 }
